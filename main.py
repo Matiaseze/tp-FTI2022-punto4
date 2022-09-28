534c46bd3390aa7cdd73a3833259a677de9bfda8
Matias <matyaseze777@gmail.com>
2022-09-28 16:38:39 -0300

    reccoriendo automata

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import re
 import pandas as pd
 import openpyxl 
+from openpyxl import Workbook
 
-book = openpyxl.load_workbook('prueba.xlsx', data_only=True)
+book = openpyxl.load_workbook('prueba.xlsx')
+sheet = book.active
+a=sheet['B1'].value
+b=sheet['C1'].value
+s0=sheet['A2'].value
+s1=sheet['A3'].value
+s2=sheet['A4'].value
+s3=sheet['A5'].value
 
-hoja = book.active
+cadena = [b,b]
+ 
+estados={s0:{a:s0,b:s1},s1:{a:s1,b:s2},s2:{a:s2,b:s3},s3:{a:s3,b:s3}}
 
-celdas = hoja ['A1' : 'B1' : 'C1']
-
-for fila in celdas: 
-
-    print ([celda.value for celda in fila])
-
-
-estados={"e0":{"a":"e0","b":"e1"},"e1":{"a":"e1","b":"e2"},"e2":{"a":"e2","b":"e3"}}
-cadena=["a","a","b","a","b","b"]
 valores=list(estados.values())
 print(valores)
-estado="e0"
-
-while estado != "e3":
+estado=s0
+print(len(cadena))
+for caracter in cadena:
     estado=estados[estado]
     print(f"estas en:{estado}")
-    estado=estado[cadena.pop()]
-
-print("legaste a un estado terminal rey")
+    estado=estado[caracter]
+if(estado == s1 or estado == s3):   
+    print("el automata acepta la cadena")
+else:
+    print("el automata no acepta la cadena")
