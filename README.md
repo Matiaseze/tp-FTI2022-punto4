f12928341227bac77dc37ece43456fad1ea84358
Matias <matyaseze777@gmail.com>
2022-09-30 02:31:51 -0300

    validando entrada y mejorando codigo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # tp-FTI2022-punto4
-Para Fundamentos
+Trabajo Practico Nº1
+Grupo 1: Arnaiz Rodrigo, Barea Matias
 
-Consigna:
+4) Consigna:
 Realice un programa que permita saber si una cadena es aceptada o no por un AFD. Consejo: Utilice una tabla transicion para su implementacion.
-Hola si estoy editando
 
--- main.py
+++ main.py
@@ -1,30 +1,78 @@
 import re
 import pandas as pd
 import openpyxl 
-from openpyxl import Workbook
 
-book = openpyxl.load_workbook('prueba.xlsx')
-sheet = book.active
-a=sheet['B1'].value
-b=sheet['C1'].value
-s0=sheet['A2'].value
-s1=sheet['A3'].value
-s2=sheet['A4'].value
-s3=sheet['A5'].value
+#Metodos
+def leer_libro(path):
+    return pd.read_excel(path)
 
-cadena = [b,b]
- 
-estados={s0:{a:s0,b:s1},s1:{a:s1,b:s2},s2:{a:s2,b:s3},s3:{a:s3,b:s3}}
+def estado_incial(libro):
+    return libro['Estados'].values[0]
 
-valores=list(estados.values())
-print(valores)
-estado=s0
-print(len(cadena))
-for caracter in cadena:
-    estado=estados[estado]
-    print(f"estas en:{estado}")
-    estado=estado[caracter]
-if(estado == s1 or estado == s3):   
-    print("el automata acepta la cadena")
-else:
-    print("el automata no acepta la cadena")
+def dame_lista_estados(libro):
+    return list(libro['Estados'].values)
+
+def filtrar_aceptadores(libro):
+    return libro[libro['Aceptador'] == "si"]
+
+def filtrar_no_aceptadores(libro):
+    return libro[libro['Aceptador'] == "no"]
+
+def construir_diccionario(estados, libro):
+    diccionario_estados = {}
+    for i in range(len(estados)):
+            dic = { "a" : libro['A'].values[i] , "b" : libro['B'].values[i] }
+            diccionario_estados[libro['Estados'].values[i]] = dic
+    return diccionario_estados
+
+def validacion_cadena(cadena):
+    regex_cadena = re.compile(r"[a-b]*")
+    return re.fullmatch(regex_cadena, cadena)
+
+
+#MAIN
+def main():
+    libro = leer_libro("./prueba.xlsx")
+
+    estado = estado_incial(libro) #estado inicial
+
+    estados = dame_lista_estados(libro)
+
+    
+    cadena = input("Ingrese cadena a validar: ") 
+    if validacion_cadena(cadena): 
+        print(f"'{cadena}' Es valida!") 
+    else: 
+        print(f"'{cadena}'No es valida!") 
+
+    # regex_estado = re.compile(r"[s-0-9]*") 
+    # estado = input("Ingrese cadena a validar: ") 
+
+    # if re.fullmatch(regex_estado, estado): 
+    #     print(f"'{estado}' Es valido!") 
+    # else: 
+    #     print(f"'{estado}'No es valido!") 
+
+    diccionario_estados = construir_diccionario(estados,libro)
+
+    filtro_aceptadores= filtrar_aceptadores(libro)
+    filtro_no_aceptadores = filtrar_no_aceptadores(libro)
+    estados_aceptadores = list(filtro_aceptadores['Estados'])
+    estados_no_aceptadores = list(filtro_no_aceptadores['Estados'])
+
+    for caracter in cadena:
+        if estado == "B":
+            break
+        val_estado=diccionario_estados[estado]
+        print(f"Caminos posibles:{val_estado}")
+        estado=val_estado[caracter]
+        print(f"Me voy por estado:{estado}")
+    
+    if(estado in estados_aceptadores):   
+        print("el automata acepta la cadena")
+    else:
+        print("el automata no acepta la cadena")
+
+
+if __name__ == "__main__":
+    main()
