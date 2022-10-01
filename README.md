a1f775027666d828d1c0a9438d59eec08ecc9ef9
matias <matyaseze777@gmail.com>
2022-09-30 23:21:50 -0300

    actualizacion de comentarios

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Grupo 1: Arnaiz Rodrigo, Barea Matias
 4) Consigna:
 Realice un programa que permita saber si una cadena es aceptada o no por un AFD. Consejo: Utilice una tabla transicion para su implementacion.
 
+Programa:
+
+Este programa permite saber si una cadena es aceptada o no por un AFD. El automata esta en una tabla de transicion en excel, el cual el usuario puede editarlo (manteniendo la estructura) para poder cambiar de AFD. 
+
+Uso:
+1) Leer el archivo INSTRUCCIONES para su uso
+2) Al estar el programa iniciado,en la terminal coloque el alfabeto a verificar, si no ingresa algo valido la ejecucion finaliza
+3) El programa muestra por los distintos estados que va recorriendo cada caracter del alfabeto
+4) Al finalizar muestra si el alfabeto es aceptado (es decir termino en un estado terminal) o no es aceptado (llego al estado Basura) por el AFD
--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 import re
 import pandas as pd
-import openpyxl 
 
 #Metodos
 def leer_libro(path):
     return pd.read_excel(path)
 
 def estado_incial(libro):
-    return libro['Estados'].values[0]
+    return libro["Estados"].values[0]
 
 def dame_lista_estados(libro):
-    return list(libro['Estados'].values)
+    return list(libro["Estados"].values)
 
 def filtrar_aceptadores(libro):
-    return libro[libro['Aceptador'] == "si"]
-
-def filtrar_no_aceptadores(libro):
-    return libro[libro['Aceptador'] == "no"]
+    return libro[libro["Aceptador"] == "si"]
 
 def construir_diccionario(estados, libro):
     diccionario_estados = {}
     for i in range(len(estados)):
-            dic = { "a" : libro['A'].values[i] , "b" : libro['B'].values[i] }
-            diccionario_estados[libro['Estados'].values[i]] = dic
+            dic = { 'a' : libro['A'].values[i] , 'b' : libro['B'].values[i] }
+            diccionario_estados[libro["Estados"].values[i]] = dic
     return diccionario_estados
 
 def validacion_cadena(cadena):
@@ -38,41 +34,29 @@ def main():
 
     estados = dame_lista_estados(libro)
 
-    
     cadena = input("Ingrese cadena a validar: ") 
     if validacion_cadena(cadena): 
         print(f"'{cadena}' Es valida!") 
+    
+        diccionario_estados = construir_diccionario(estados,libro)
+
+        filtro_aceptadores= filtrar_aceptadores(libro)
+        estados_aceptadores = list(filtro_aceptadores['Estados'])
+
+        for caracter in cadena:
+            if estado == "B":
+                break
+            val_estado=diccionario_estados[estado]
+            print(f"Caminos posibles:{val_estado}")
+            estado=val_estado[caracter]
+            print(f"Me voy por estado:{estado}")
+        
+        if(estado in estados_aceptadores):   
+            print("el automata acepta la cadena")
+        else:
+            print("el automata no acepta la cadena")    
     else: 
         print(f"'{cadena}'No es valida!") 
-
-    # regex_estado = re.compile(r"[s-0-9]*") 
-    # estado = input("Ingrese cadena a validar: ") 
-
-    # if re.fullmatch(regex_estado, estado): 
-    #     print(f"'{estado}' Es valido!") 
-    # else: 
-    #     print(f"'{estado}'No es valido!") 
-
-    diccionario_estados = construir_diccionario(estados,libro)
-
-    filtro_aceptadores= filtrar_aceptadores(libro)
-    filtro_no_aceptadores = filtrar_no_aceptadores(libro)
-    estados_aceptadores = list(filtro_aceptadores['Estados'])
-    estados_no_aceptadores = list(filtro_no_aceptadores['Estados'])
-
-    for caracter in cadena:
-        if estado == "B":
-            break
-        val_estado=diccionario_estados[estado]
-        print(f"Caminos posibles:{val_estado}")
-        estado=val_estado[caracter]
-        print(f"Me voy por estado:{estado}")
-    
-    if(estado in estados_aceptadores):   
-        print("el automata acepta la cadena")
-    else:
-        print("el automata no acepta la cadena")
-
 
 if __name__ == "__main__":
     main()
